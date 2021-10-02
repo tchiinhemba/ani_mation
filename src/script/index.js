5a542c6a1472284329c04cc1f9e08375160b5b44
eladioclaudio <elladioclaudio@gmail.com>
2021-10-02 09:00:04 +0100

    refactoring app project

--- src/script/index.js
+++ src/script/index.js
@@ -3,7 +3,7 @@ let github = document.querySelector('#link');
 
 
 const link = {
-    github: 'https://github.com/tchiinhemba'
+    github: 'https://github.com/eladioclaudio'
 };
 
 github.addEventListener('click', () => document.location.assign(link.github));
